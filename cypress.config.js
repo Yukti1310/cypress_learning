d5a52aa1eeddee13fb4a401a45afc7511885c9be
Yukti Baheti <yukti_baheti@external.mckinsey.com>
2025-05-31 17:17:21 +0530

    test case 2

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,20 @@
 const { defineConfig } = require("cypress");
 
+
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'Cypress Learning Report',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
-  watchForFileChanges:true
+  watchForFileChanges: true,
+  video: false
 });
